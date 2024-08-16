228fd0cceb3928bfc459cf1d9ffa119ef321e29b
Ingrid Dahiana Enriquez Dos Santos <ingridenriquez74@gmail.com>
2024-08-16 19:34:06 -0400

    add: Primer formulario

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from "react";
+import ReactDOM from 'react-dom/client'; 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    function MyForm() {
+        const [inputs, setInputs] = useState({});
+        
+        const handleChange = (event) => {
+            const name = event.target.name;
+            const value = event.target.value;
+            setInputs(values => ({...values, [name]: value}))
+        }
+        
+        const handleSubmit = (event) => {
+            event.preventDefault();
+            alert(`Tu pedido realizado fue de: ${inputs}`);
+        }
+        
+        return (
+            <form onSubmit={handleSubmit}>
+            <label>Cliente:
+            <input 
+              type="text" 
+              name="nombre" 
+              value={inputs.nombre || ""} 
+              onChange={handleChange}
+              />
+            <label>Tipo de ramo:
+            <input 
+              type="text" 
+              name="tiporamo" 
+              value={inputs.tiporamo || ""} 
+              onChange={handleChange}
+              />
+            </label>
+            </label>
+            <label>Precio:
+              <input 
+                type="number" 
+                name="precio" 
+                value={inputs.precio || ""} 
+                onChange={handleChange}
+            />
+            </label>
+            <label>Direccion de envio:
+              <input 
+                type="text" 
+                name="direccion" 
+                value={inputs.direccion || ""} 
+                onChange={handleChange}
+            />
+            </label>
+        
+            <input type="submit" />
+          </form>
+        )
+    }
+    
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<MyForm />);
+}
 export default App;
